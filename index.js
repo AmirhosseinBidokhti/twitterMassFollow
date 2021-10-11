5bb5f0c29b092b37aa0a99c874fadfd2ed525407
AmirhosseinBidokhti <amir.bidokhti99@gmail.com>
2021-10-12 01:50:48 +0330

    first commit

--- index.js
+++ index.js
@@ -1,52 +1,37 @@
 const { Builder, By, Capabilities, until } = require("selenium-webdriver");
 
-const AUTH_TOKEN = "8510ea2dada9442453dc5408f7fd690d2d1dbace";
+const AUTH_TOKEN = "EXTRACT_THIS_FROM_YOUR_BROWSER_COOKIE_ONCE_YOU_LOGGED_IN";
+
 const FOLLOW_BTN_XPATH =
-  "/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/div[1]/div/div[1]/div/div[3]/div/div/div/span/span";
+  "/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/div[1]/div/div[1]/div/div[2]/div/div";
 
-const caps = new Capabilities();
-caps.setPageLoadStrategy("eager");
+// const caps = new Capabilities();
+// caps.setPageLoadStrategy("eager");
 
-async function example() {
-  let driver = await new Builder()
-    .withCapabilities(caps)
-    .forBrowser("chrome")
-    .build();
-
+async function follow() {
   try {
+    let driver = await new Builder().forBrowser("chrome").build();
+
     await driver.get("https://twitter.com/");
+
     await driver.manage().addCookie({
       name: "auth_token",
       value: AUTH_TOKEN,
     });
 
+    // TODO: read a file full of accounts to follow
     await driver.get("https://twitter.com/Codecancare");
 
-    await sleep(5000);
+    await driver.sleep(3000);
 
-    let followBtn = await driver
-      .wait(until.elementLocated(By.xpath(FOLLOW_BTN_XPATH)), 6000)
-      .click();
-    //await driver.manage().setTimeouts({ implicit: 3000 });
-    //let followBtn = await driver.findElement(By.xpath(FOLLOW_BTN_XPATH));
-    // let foo = await driver.wait(
-    //   until.elementLocated(By.xpath(FOLLOW_BTN_XPATH)),
-    //   30000,
-    //   "Timed out after 30 seconds",
-    //   5000
-    // );
+    let followBtn = driver.findElement(By.xpath(FOLLOW_BTN_XPATH));
+    followBtn.click();
 
-    // await foo.click();
-
-    //await followBtn.click();
+    ///await driver.quit();
   } catch (error) {
-    console.log(error);
-    //example();
+    console.error("-----------oops!-----------");
+    console.error(error);
   }
 }
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-example();
+//follow();
